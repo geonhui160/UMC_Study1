3670d973fb3eb786d7c6b07d7e4d5a73c74a6069
Geon <jcv31312@naver.com>
2024-11-23 21:08:02 +0900

    FEAT: week8실습_api

--- src/main/java/umc/study/validation/validator/CategoriesExistValidator.java
+++ src/main/java/umc/study/validation/validator/CategoriesExistValidator.java
@@ -4,6 +4,8 @@ import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import umc.study.apiPayload.code.status.ErrorStatus;
+import umc.study.repository.FoodCategoryRepository;
 import umc.study.validation.annotation.ExistCategories;
 
 import java.util.List;
@@ -12,6 +14,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class CategoriesExistValidator implements ConstraintValidator<ExistCategories, List<Long>> {
 
+    private final FoodCategoryRepository foodCategoryRepository;
 
     @Override
     public void initialize(ExistCategories constraintAnnotation) {
@@ -19,7 +22,16 @@ public class CategoriesExistValidator implements ConstraintValidator<ExistCatego
     }
 
     @Override
-    public boolean isValid(List<Long> value, ConstraintValidatorContext context) {
-        return false;
+    public boolean isValid(List<Long> values, ConstraintValidatorContext context) {
+        boolean isValid = values.stream()
+                .allMatch(value -> foodCategoryRepository.existsById(value));
+
+        if (!isValid) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(ErrorStatus.FOOD_CATEGORY_NOT_FOUND.toString()).addConstraintViolation();
+        }
+
+        return isValid;
+
     }
 }
